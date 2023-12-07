e6ed0c357e04bc985c005fc6769a0dcdfb10ca35
Serhii Yefimov <sergii.iefimov@gmail.com>
2023-12-07 12:43:25 +0200

    udaped redux store

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,11 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import type { PreloadedState } from '@reduxjs/toolkit';
 import authReducer from './auth/authSlice';
 
-export const store = configureStore({
-  reducer: {
-    users: authReducer,
-  },
+// Create the root reducer independently to obtaine the RootState type
+const rootReducer = combineReducers({
+  users: authReducer,
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export function setupStore(preloadedState?: PreloadedState<RootState>) {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+}
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
